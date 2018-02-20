9403834c9f6f1dbf9ec6f47aa513632e40604d98
Acronptel <36438683+Acronptel@users.noreply.github.com>
2018-02-20 14:59:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/kishan',function (req,res) {
+    res.sendFile("hey i am kishan");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
