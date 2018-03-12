951f5c1bcdf6dd9d85700e30c9b373e06d602a4f
Acronptel <36438683+Acronptel@users.noreply.github.com>
2018-03-12 14:53:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+
+var config = {
+user : 'acronptel',
+database: 'acronptel',
+host: 'db.imad.hasura-app.io',
+port :'5432',
+password: process.env.DB_PASSWORD
+
+}
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -21,8 +32,18 @@ app.get('/', function (req, res) {
     app.get('/a3',function (req,res) {
         res.send('hey i am kishan3');
     });
-
-
+var pool = new Pool(config);
+app.get('/test-db',function(req,res) {
+// Make a select request
+// return a response with the results
+pool.query("SELECT * FROM test",function(err,result) {
+if(err) {
+res.status(500).send(err.toString());
+}else {
+res.send(JSON.stringify(result));
+}
+});
+});
 
 
 app.get('/ui/style.css', function (req, res) {
