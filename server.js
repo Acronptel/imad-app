3bc821099db8036d62695ee5edf9ad2e4f31a4b1
Acronptel <36438683+Acronptel@users.noreply.github.com>
2018-03-12 15:50:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,9 +20,21 @@ app.get('/', function (req, res) {
 });
 
 
-    app.get('/a1',function (req,res) {
-        res.send('hey i am kishan1');
-    });
+app.get('/articles/:articleName', function (req, res) {
+// res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+pool.query("SELECT * from Articles where title='" + req.params.articleName + "'",function(err,result) {
+if(err) {
+res.status(500).send(err.toString());
+}else {
+if(result.rows.length===0) {
+res.status(404).send("Article Not Found");
+}else {
+var articleData = result.rows[0];
+res.send(createTemplate(articleData));
+}
+}
+});
+}); 
     
     app.get('/a2',function (req,res) {
         res.send('hey i am kishan2');
